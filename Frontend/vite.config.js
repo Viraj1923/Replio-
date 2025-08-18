2fddc4f07d167439c00cc026995b2f2a75044aa0
Viraj Mulik <virajmulik2304@gmail.com>
2025-08-18 23:00:27 +0530

    vite

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,8 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "./", // 👈 this ensures assets resolve correctly on Vercel
   server: {
     host: "0.0.0.0",
-    port: 5173, // or any free port
+    port: 5173,
   },
 });
